e079f8d6e39f0d4044c16e311416853f830faba2
Fhaddad9 <ferashaddadgit@gmail.com>
2022-05-06 13:55:42 +0100

    Spring Initializr added

---
+++ Online-Microservice/src/main/java/com/FHaddad/OnlineMicroservice/OnlineMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.FHaddad.OnlineMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineMicroserviceApplication.class, args);
+	}
+
+}
---
+++ Online-Microservice/src/test/java/com/FHaddad/OnlineMicroservice/OnlineMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FHaddad.OnlineMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
